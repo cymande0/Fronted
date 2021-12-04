d3c77f88b7a2a214d290b0d49579bfe49872be9c
cymande0 <konradpietapl@gmail.com>
2021-12-04 08:33:16 +0100

    Loop practice

---
+++ JS/Praktyka/Projekt3Petle/Zadanie1/js.js
@@ -0,0 +1,27 @@
+
+console.log("elo");
+const btn = document.querySelector("button");
+const li = document.querySelectorAll("li");
+let size = 10;
+
+function initial() {
+    for(let i = 0; i < li.length; i++){
+        li[i].style.fontSize = `${size}px`;
+    }
+}
+
+function changeSizeOfLi (){
+    size++;
+    li.forEach(element => {
+        element.style.display = "block";
+        element.style.fontSize = `${size}px`;
+    });
+    // for(let i = 0; i < li.length; i++){
+    //     const tempLi = li[i];
+    //     tempLi.style.display = "block";
+    //     tempLi.style.fontSize = size + "px";
+    // }
+}
+
+initial()
+btn.addEventListener("click", changeSizeOfLi)
